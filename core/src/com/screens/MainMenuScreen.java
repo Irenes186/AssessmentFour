d670b24e7d313535f5a4ee9c3b380052cfe692a7
AngrySaltyFish <james.rand1@aol.co.uk>
2020-04-10 13:31:35 +0100

    Allowing choice of loaded file

--- core/src/com/screens/MainMenuScreen.java
+++ core/src/com/screens/MainMenuScreen.java
@@ -127,7 +127,26 @@ public class MainMenuScreen implements Screen {
 		TextButton playButton = new TextButton("Play", skin);
 		TextButton howToPlayButton = new TextButton("How to Play", skin);
 		TextButton quitButton = new TextButton("Quit", skin);
+
+    File saveDir = new File ("saves/");
     TextButton loadButton = new TextButton("Load", skin);
+    SelectBox <String> saveSelect = new SelectBox <String>(skin);
+
+    File[] files = saveDir.listFiles();
+    int numberOfFiles = files.length;
+    String[] fileNames = new String [numberOfFiles];
+
+    for (int i = 0; i < numberOfFiles; i++) {
+        fileNames[i] = files[i].getName();
+    }
+
+    saveSelect.setItems(fileNames);
+
+    Table saveTable = new Table();
+    saveTable.add(loadButton).padBottom(20).width(100).height(40);
+    saveTable.add(saveSelect).padBottom(20).width(100).height(40).right();
+
+    System.out.println(saveSelect.getItems());
 
 		// Add buttons to table and style them
 		buttonTable.add(heading).padBottom(10);
@@ -135,8 +154,8 @@ public class MainMenuScreen implements Screen {
 		buttonTable.add(subHeading).padBottom(20);
 		buttonTable.row();
 		buttonTable.add(playButton).padBottom(20).width(200).height(40);
-                buttonTable.row();
-                buttonTable.add(loadButton).padBottom(20).width(200).height(40);
+    buttonTable.row();
+    buttonTable.add(saveTable);
 		buttonTable.row();
 		buttonTable.add(howToPlayButton).padBottom(20).width(200).height(40);
 		buttonTable.row();
@@ -171,8 +190,12 @@ public class MainMenuScreen implements Screen {
     loadButton.addListener(new ClickListener() {
       @Override
 		  public void clicked(InputEvent event, float x, float y) {
-        File file = new File ("saves/test.txt");
         String fileString;
+        String fileName = saveSelect.getSelected();
+        if (fileName == null) {
+            return;
+        }
+        File file = new File("saves/" + fileName);
         ArrayList<String> fileContents = new ArrayList<String>();
 
         try {
